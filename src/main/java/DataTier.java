8d38739d4c49d80a8039c95ab0bc32605cc4c473
Mher <poghosyanusc@gmail.com>
2018-02-01 18:38:20 -0800

    Resubmitting because I forgot to change author

--- src/main/java/DataTier.java
+++ src/main/java/DataTier.java
@@ -4,12 +4,15 @@
  * Implement the appropriate methods for this tier below.
  */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public class DataTier {
 
 	private String fileName;
-	
+
 	public DataTier(String inputSource) {
 		fileName = inputSource;
 	}
@@ -22,6 +25,21 @@ public class DataTier {
 	 * @return
 	 */
 	public List<Book> getAllBooks() {
+		FileInputStream reader = null;
+
+		try {
+			reader = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		return null;
 	}
 }
