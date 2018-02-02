df8459141032db934acb6c355418c1b90b42203e
mpoghosyan <mher.poghosyan@loni.usc.edu>
2018-02-01 18:32:00 -0800

    Opened file and handled thrown exception, need to test

--- src/main/java/DataTier.java
+++ src/main/java/DataTier.java
@@ -4,6 +4,9 @@
  * Implement the appropriate methods for this tier below.
  */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public class DataTier {
@@ -22,6 +25,22 @@ public class DataTier {
 	 * @return
 	 */
 	public List<Book> getAllBooks() {
+		FileInputStream reader = null;
+
+		try {
+			reader = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 		return null;
 	}
 }
